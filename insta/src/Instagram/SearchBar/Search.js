d6de749b8f7af1ecd5bcc70a90e65bc7608bf5d7
Mayankho <yankhot96@gmail.com>
2019-03-28 23:45:29 -0400

    Project Start and component routing

---
+++ insta/src/Instagram/SearchBar/Search.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Search extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            searchbar : []
+        }
+    }
+    render(){
+        return(
+            // Need to render an input form
+            <div>
+                <p>hey</p> 
+                
+            </div>
+        )
+    }
+}
+
+
+export default Search
